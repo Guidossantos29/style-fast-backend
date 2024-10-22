37c590560d645b42b8f7d4f115391d9a139e921a
Luiz Guilherme <lguidossantos29@gmail.com>
2024-10-22 17:53:29 -0300

    created service of login and register

---
+++ src/services/authServiceUser.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { error } from "console";
+import jwt from "jsonwebtoken"
+
+const prisma = new PrismaClient();
+
+class authRegisterUser {
+    async registerUser(email: string, password: string) {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        return prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword
+            },
+        })
+    }
+
+    async loginUser(email: string, password: string) {
+        const user = await prisma.user.findUnique({ where: { email } })
+
+        if(!user || !(await bcrypt.compare(password,user.password) ))
+        {
+            throw new Error("Invalid credentials")
+        }
+         const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET as string,{
+            expiresIn:"1h"
+         })
+    }
+
+
+    
+
+
+}
+
+export default authRegisterUser
+
