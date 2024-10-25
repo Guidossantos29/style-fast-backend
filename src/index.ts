f464b40c885bc2db6f445d312cc2dd7716421828
Luiz Guilherme <lguidossantos29@gmail.com>
2024-10-25 20:31:44 -0300

    created swagger config

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ import { Prisma, PrismaClient } from "@prisma/client";
 dotenv.config();
 const app = Express();
 const prisma = new PrismaClient();
-
+const PORT = process.env.PORT
 
 app.use(cors());
 
-app.listen(3000,() => {
+app.listen(PORT,() => {
     console.log("server online local");
     
 })
